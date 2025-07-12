baf6573c16d7b9ebc9901032a5b90954d24d5f21
vilich <vilich96m@gmail.com>
2025-07-12 16:27:11 +0200

    add

---
+++ src/components/TodoForm/TodoForm.tsx
@@ -0,0 +1,113 @@
+import { ChangeEvent, useState } from 'react';
+import usersFromServer from '../../api/users';
+// import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+type Props = {
+  onSubmit: (data: { title?: string; userId: number }) => void;
+};
+
+function getUserById(userId: number): User | undefined {
+  return usersFromServer.find((user: User) => user.id === userId);
+}
+
+function getRandomDigits() {
+  return Math.random().toFixed(16).slice(2);
+}
+
+export const TodoForm: React.FC<Props> = ({ onSubmit }) => {
+  const [id] = useState(() => `${name}-${getRandomDigits()}`);
+
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [userId, setUserId] = useState(0);
+  const [userIdError, setUserIdError] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setTitleError(!title.trim());
+    setUserIdError(!userId);
+
+    const user = getUserById(userId);
+
+    if (title.trim() && userId) {
+      if (!user) {
+        setUserIdError(true);
+
+        return;
+      }
+
+      onSubmit({
+        title: title.trim(),
+        userId,
+      });
+
+      setTitle('');
+      setUserId(0);
+    }
+  };
+
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setTitle(value);
+
+    if (titleError && value.trim()) {
+      setTitleError(false);
+    }
+  };
+
+  const handleUserChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = +e.target.value;
+
+    setUserId(value);
+
+    if (userIdError && value !== 0) {
+      setUserIdError(false);
+    }
+  };
+
+  return (
+    <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
+      <div className="field">
+        <label htmlFor={id}>Title: </label>
+        <input
+          type="text"
+          id={id}
+          data-cy="titleInput"
+          placeholder="Enter a title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        {titleError && <span className="error">Please enter a title</span>}
+      </div>
+
+      <div className="field">
+        <label htmlFor="selectChoose">User: </label>
+        <select
+          id="selectChoose"
+          data-cy="userSelect"
+          value={userId}
+          onChange={handleUserChange}
+        >
+          <option value="0" disabled>
+            Choose a user
+          </option>
+
+          {usersFromServer.map((user: User) => (
+            <option value={user.id} key={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        {userIdError && <span className="error">Please choose a user</span>}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+import React from 'react';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo user={todo.user} />
+  </article>
+);
